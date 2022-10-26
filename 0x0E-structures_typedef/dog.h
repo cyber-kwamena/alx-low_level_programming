6667355066798745ad525d7d112fe20438e406b0
Kwamena <83978246+Jake186@users.noreply.github.com>
2022-10-26 06:20:13 +0000

    Update dog.h
    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  */
 
 /**
- * struct dog - A new type describing a dog.
+ * struct dog - A new type describing a dog
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
